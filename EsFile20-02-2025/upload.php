8e1c411ea694fa5fb5821d4ad64275b0e4808f9a
MarcoBitto <marco.bitto@iisvittorioveneto.it>
2025-02-19 17:50:30 +0100

    boh provato a fare robe

--- EsFile20-02-2025/upload.php
+++ EsFile20-02-2025/upload.php
@@ -2,4 +2,15 @@
 
 if(!isset($_POST["send"])){
     echo "<form action = {$_SERVER['PHP_SELF']} enctype='multipart/form-data' method='post'>";
+    echo "<input type='file' name='file'>";
+    echo "<input type='submit' name='upload' value='upluoda'>";
+    echo "</form>";
 }
+
+if($_SERVER["REQUEST_METHOD"] == "POST" && (isset($_POST["upload"]))){
+    $file = $_FILES;
+    echo mime_content_type($file["file"]["type"]);
+    //if($file["type"])
+}
+
+?>
